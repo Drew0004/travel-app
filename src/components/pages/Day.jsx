8b20584c7549e30a5316d037625c17088b035e4e
Drew0004 <drdrewgraphic@gmail.com>
2024-08-05 14:37:53 +0200

    Add static map in days component

--- src/components/pages/Day.jsx
+++ src/components/pages/Day.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import { APIProvider, Map, AdvancedMarker, Pin, InfoWindow } from '@vis.gl/react-google-maps';
 import Stop from './Stop';
 
 const Day = () => {
@@ -9,21 +10,36 @@ const Day = () => {
   const filteredStops = trip.stops.filter(singleStop => 
     singleStop && singleStop.stopDate === actualDay
   );
+  
+  //Dati maps api
+  const googleMapsApiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+  const myMapId = process.env.REACT_APP_MAP_ID
+  const position = {lat: 53.54, lng: 10}
 
   return (
-    <div>
-      <h1 className='text-white'>Day {dayIndex + 1}</h1>
-      <p className='text-white'>Actual Day: {actualDay}</p>
-      {filteredStops.length > 0 ? (
-        filteredStops.map((singleStop, stopIndex) => (
-          <div key={stopIndex}>
-            <Stop singleStop={singleStop} />
-          </div>
-        ))
-      ) : (
-        <div className='text-white'>Sembra non ci siano tappe...</div>
-      )}
-    </div>
+    <APIProvider apiKey={googleMapsApiKey}>
+      <div>
+        <h1 className='text-white'>Day {dayIndex + 1}</h1>
+        <p className='text-white'>Actual Day: {actualDay}</p>
+        {filteredStops.length > 0 ? (
+          filteredStops.map((singleStop, stopIndex) => (
+            <div key={stopIndex}>
+              <Stop singleStop={singleStop} />
+            </div>
+          ))
+        ) : (
+          <div className='text-white'>Sembra non ci siano tappe...</div>
+        )}
+      </div>
+      {/* Sezione Mappa */}
+      <div style={{ height: '400px' }}>
+        <Map zoom={9} center={position} mapId={myMapId}>
+            <AdvancedMarker position={position}>
+                <Pin/>
+            </AdvancedMarker>
+        </Map>
+      </div>
+    </APIProvider>
   );
 };
 
