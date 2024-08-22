1a5d1e2cfc0887a39de98d597758b1ab427ea336
Drew0004 <drdrewgraphic@gmail.com>
2024-08-22 20:54:27 +0200

    Fix form style

--- src/components/pages/AddNewTrip.jsx
+++ src/components/pages/AddNewTrip.jsx
@@ -73,38 +73,40 @@ const AddNewTrip = () => {
             <div className="col-6 form-img-background d-flex justify-content-center align-items-center p-5">
                 <h1 className='main-green fw-bold'>Aggiungi un nuovo Viaggio alla tua Avventura!</h1>
             </div>
-            <div className="col-6 d-flex justify-content-center align-items-center">
-                <form className='row g-0 p-5' onSubmit={handleNewTrip}>
+            <div className="col-6 d-flex justify-content-center align-items-center px-5">
+                <div className="form-wrapper">
+                    <form className='row g-0 p-5' onSubmit={handleNewTrip}>
 
-                    <label className='my-label mb-3' htmlFor="travelName">Nome del Viaggio</label>
-                    <input className='mb-3 rounded-5 px-4 py-2 my-input' type="text" name="travelName" placeholder='Nome' required />
-                    {errors.travelName && <p className="text-danger">{errors.travelName}</p>}
+                        <label className='my-label mb-3' htmlFor="travelName">Nome del Viaggio</label>
+                        <input className='mb-3 rounded-5 px-4 py-2 my-input' type="text" name="travelName" placeholder='Nome' required />
+                        {errors.travelName && <p className="text-danger">{errors.travelName}</p>}
 
-                    <label className='my-label mb-3' htmlFor="dateStart">Data di inizio</label>
-                    <input className='mb-3 rounded-5 px-4 py-2 my-input' type="date" name="dateStart" required />
-                    {errors.dateStart && <p className="text-danger">{errors.dateStart}</p>}
+                        <label className='my-label mb-3' htmlFor="dateStart">Data di inizio</label>
+                        <input className='mb-3 rounded-5 px-4 py-2 my-input' type="date" name="dateStart" required />
+                        {errors.dateStart && <p className="text-danger">{errors.dateStart}</p>}
 
-                    <label className='my-label mb-3' htmlFor="dateEnd">Data di fine</label>
-                    <input className='mb-3 rounded-5 px-4 py-2 my-input' type="date" name="dateEnd" required />
-                    {errors.dateEnd && <p className="text-danger">{errors.dateEnd}</p>}
+                        <label className='my-label mb-3' htmlFor="dateEnd">Data di fine</label>
+                        <input className='mb-3 rounded-5 px-4 py-2 my-input' type="date" name="dateEnd" required />
+                        {errors.dateEnd && <p className="text-danger">{errors.dateEnd}</p>}
 
-                    <label className='my-label mb-3' htmlFor="description">Descrizione del viaggio</label>
-                    <textarea className='mb-3 rounded-5 px-4 py-2 my-input' name="description" placeholder='Descrizione' required></textarea>
-                    {errors.description && <p className="text-danger">{errors.description}</p>}
+                        <label className='my-label mb-3' htmlFor="description">Descrizione del viaggio</label>
+                        <textarea className='mb-3 rounded-5 px-4 py-2 my-input' name="description" placeholder='Descrizione' required></textarea>
+                        {errors.description && <p className="text-danger">{errors.description}</p>}
 
-                    <label className='my-label mb-3' htmlFor="travelImg">Immagine</label>
-                    <input className='mb-3 form-control rounded-5 my-input' type="file" name='travelImg' required />
-                    {errors.travelImg && <p className="text-danger">{errors.travelImg}</p>}
+                        <label className='my-label mb-3' htmlFor="travelImg">Immagine</label>
+                        <input className='mb-3 form-control rounded-5 my-input' type="file" name='travelImg' required />
+                        {errors.travelImg && <p className="text-danger">{errors.travelImg}</p>}
 
-                    <div className="col-12 d-flex justify-content-around align-items-center my-4">
-                        <Link className='text-decoration-none my-secondary-btn px-4 py-2' to={'/'}>
-                            <span>Annulla</span>
-                        </Link>
-                        <button className='my-main-btn px-5 py-2' type="submit" style={{ border: 'none' }}>
-                            <span>Invia</span>
-                        </button>
-                    </div>
-                </form>
+                        <div className="col-12 d-flex justify-content-around align-items-center my-4">
+                            <Link className='text-decoration-none my-secondary-btn px-4 py-2' to={'/'}>
+                                <span>Annulla</span>
+                            </Link>
+                            <button className='my-main-btn px-5 py-2' type="submit" style={{ border: 'none' }}>
+                                <span>Invia</span>
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     );
